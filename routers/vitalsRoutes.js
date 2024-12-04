2d493edd8fd536ef6362c8bce1ebcd47021809de
Similoluwa24 <wuraolami2001@gmail.com>
2024-12-04 14:31:10 +0100

    4/12/2024

--- routers/vitalsRoutes.js
+++ routers/vitalsRoutes.js
@@ -6,7 +6,7 @@ const {isAuthenticated} = require('../middleware/auth')
 router.post('/add', isAuthenticated, vitalController.addVitals)
 router.get('/view',isAuthenticated, vitalController.getByUserId)
 router.get('/latest',isAuthenticated, vitalController.getLatest)
-router.get('/:id',isAuthenticated,vitalController.getById)
 router.get('/me', isAuthenticated, vitalController.getVitalsByUserId)
+router.get('/:id',isAuthenticated,vitalController.getById)
 
 module.exports = router
